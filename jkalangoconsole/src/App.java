a1c94e4bbea2033d61897cf32408e31356c86305
romulodf-cesar <romulodf.cesar@gmail.com>
2025-06-11 10:54:20 -0300

    ok

---
+++ jkalangoconsole/src/App.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        Formiga f = new Formiga();
+        f.mover();
+        f.atacar();
+
+        //objetos
+        //objeto  kalango
+        // abelhinha
+        //movendo
+        //atacando (novidade)
+       
+    }
+}
