dbd994cc433cb908400e68b01c22c04c20d61a58
romulodf-cesar <romulodf.cesar@gmail.com>
2025-06-13 10:07:09 -0300

    ok

--- jkalangoconsole/src/br/com/jkalango/view/JMissaoObjeto.java
+++ jkalangoconsole/src/br/com/jkalango/view/JMissaoObjeto.java
@@ -1,6 +1,14 @@
 package br.com.jkalango.view;
+import javax.swing.JFrame;
+import br.com.jkalango.bo.IColor;
 
-public class JMissaoObjeto {
+public class JMissaoObjeto extends JFrame implements IColor {
+
+     @Override
+     public void mudarFundo() {
+          // TODO Auto-generated method stub
+          throw new UnsupportedOperationException("Unimplemented method 'mudarFundo'");
+     }
 
     /*
      * Com os conhecimentos que apredemos
