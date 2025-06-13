4626e2874714df9e68db083fe5da37a1af1d058d
romulodf-cesar <romulodf.cesar@gmail.com>
2025-06-13 09:41:16 -0300

    avaliação poo

---
+++ jkalangoconsole/src/br/com/jkalango/view/JMissaoObjeto.java
@@ -0,0 +1,39 @@
+package br.com.jkalango.view;
+
+public class JMissaoObjeto {
+
+    /*
+     * Com os conhecimentos que apredemos
+     * no curso de POO desenvolver um 
+     * JFrame criativo.
+     * 
+     * -Construtor
+     * -JButton
+     * -JLabel e variantes (Texto)
+     * -Contar um história de terror
+     * 
+     * 
+     * Objeto
+          Classe
+          Construtor
+          Método
+          Herança
+          Atributos
+          Polimorfismo
+          Encapsulamento
+          Interfaces
+          Abstrato
+          Classe abstrato
+          Método Abstrato 
+     * 
+     * 
+     * 
+     * Componentes autorizados:
+     * JLabel
+     *    -cores
+     *    -fontes
+     *    -embutir HTML
+     */
+
+
+}
